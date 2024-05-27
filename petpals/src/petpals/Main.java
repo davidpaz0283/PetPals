43d29be845eab9b6503c4a2fdbe7946313a170e8
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2024-05-27 19:12:15 -0300

    Class Cuidadores

--- petpals/src/petpals/Main.java
+++ petpals/src/petpals/Main.java
@@ -9,5 +9,7 @@ public static void main (String [] args) {
 	ImageIcon iconBienvenidos = new ImageIcon(Main.class.getResource("/images/petpals.png"));
 	JOptionPane.showMessageDialog(null, "", "Bienvenidos ", 0, iconBienvenidos);
 	
+	ImageIcon iconDespedida = new ImageIcon(Main.class.getResource("/images/gracias.jpg"));
+	JOptionPane.showMessageDialog(null, "","Gracias por confiar en nosotros", 0, iconDespedida);
 }
 }
